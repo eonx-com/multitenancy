b58113cbfae13deba0f0c1fd4f1d691b528679f1
Edward Murrell <edward@loyaltycorp.com.au>
2019-07-09 10:45:10 +1000

    PYMT-972: Implement name setter.

--- src/Database/Entities/Tenant.php
+++ src/Database/Entities/Tenant.php
@@ -50,4 +50,13 @@ class Tenant
     public function getName(): string {
         return $this->name;
     }
+
+    /**
+     * Update common name of the tenant.
+     *
+     * @param string $name
+     */
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
 }

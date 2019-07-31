67eed1bba47be9b4e05c968227d217bdb3478d07
Rashmit Singh <rashmit@loyaltycorp.com.au>
2019-07-31 13:42:12 +1000

    created a few stubs to use in tests.

---
+++ tests/Stubs/Vendor/External/Auth/AuthStub.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\LoyaltyCorp\Multitenancy\Stubs\Vendor\External\Auth;
+
+use EoneoPay\Externals\Auth\Interfaces\AuthInterface;
+use EoneoPay\Externals\ORM\Interfaces\EntityInterface;
+use Tests\LoyaltyCorp\Multitenancy\Stubs\Vendor\External\ORM\UserStub;
+
+/**
+ * @coversNothing
+ */
+class AuthStub implements AuthInterface
+{
+    /**
+     * @var \Tests\LoyaltyCorp\Multitenancy\Stubs\Vendor\External\ORM\UserStub
+     */
+    private $user;
+
+    /**
+     * AuthStub constructor.
+     *
+     * @param \Tests\LoyaltyCorp\Multitenancy\Stubs\Vendor\External\ORM\UserStub|null $user
+     */
+    public function __construct(?UserStub $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function user(): ?EntityInterface
+    {
+        return $this->user;
+    }
+}

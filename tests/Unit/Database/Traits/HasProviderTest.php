dad8be6d0d9b338a0d900ea1c6c59ccf1ba2125b
Brendon Tyree <brendon@tyree.co.nz>
2019-08-29 14:23:17 +1000

    PYMT-1085: Updated HasProvider trait test

--- tests/Unit/Database/Traits/HasProviderTest.php
+++ tests/Unit/Database/Traits/HasProviderTest.php
@@ -16,10 +16,12 @@ class HasProviderTest extends AppTestCase
      * Tests that the getter returns the same provider instance as what was set through the setter.
      *
      * @return void
+     *
+     * @throws \ReflectionException
      */
     public function testTraitGetterSetter(): void
     {
-        $provider = new Provider('test-provider', 'Test Provider');
+        $provider = $this->getProvider();
         $dummy = new class
         {
             use HasProvider;
@@ -27,5 +29,46 @@ class HasProviderTest extends AppTestCase
         $dummy->setProvider($provider);
 
         self::assertSame($provider, $dummy->getProvider());
+    }
+
+    /**
+     * Tests that when a provider is set, the `providerId` property in the class using the trait has its value updated
+     * to match that of the provider id.
+     *
+     * @return void
+     *
+     * @throws \ReflectionException
+     */
+    public function testTraitProviderIdSet(): void
+    {
+        $provider = $this->getProvider();
+        $dummy = new class
+        {
+            use HasProvider;
+        };
+        $dummy->setProvider($provider);
+        $property = (new \ReflectionClass($dummy))->getProperty('providerId');
+        $property->setAccessible(true);
+
+        self::assertSame(123, $property->getValue($dummy));
+    }
+
+    /**
+     * Creates a new provider entity instance and sets its id through reflection.
+     *
+     * @return \LoyaltyCorp\Multitenancy\Database\Entities\Provider
+     *
+     * @throws \ReflectionException
+     */
+    private function getProvider(): Provider
+    {
+        $provider = new Provider('test-provider', 'Test Provider');
+
+        $reflection = new \ReflectionClass($provider);
+        $property = $reflection->getProperty('providerId');
+        $property->setAccessible(true);
+        $property->setValue($provider, 123);
+
+        return $provider;
     }
 }

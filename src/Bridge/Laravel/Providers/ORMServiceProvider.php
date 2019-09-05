935d838ee0dda7df539b2ccfecae9d56cf573800
Scott Dawson <scott@loyaltycorp.com.au>
2019-09-05 16:03:52 +1000

    Add orm service provider

---
+++ src/Bridge/Laravel/Providers/ORMServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace LoyaltyCorp\Multitenancy\Bridge\Laravel\Providers;
+
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\ServiceProvider;
+use LoyaltyCorp\Multitenancy\Externals\Interfaces\ORM\EntityManagerInterface;
+use LoyaltyCorp\Multitenancy\Externals\ORM\EntityManager;
+
+class ORMServiceProvider extends ServiceProvider
+{
+    /**
+     * @noinspection PhpMissingParentCallCommonInspection Parent implementation is empty
+     *
+     * {@inheritdoc}
+     */
+    public function register(): void
+    {
+        $this->app->bind(EntityManagerInterface::class, static function (Container $app): EntityManager {
+            $entityManager = $app->make('registry')->getManager();
+
+            return new EntityManager($entityManager);
+        });
+    }
+}
---
+++ tests/Unit/Bridge/Laravel/Providers/ORMServiceProviderTest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\LoyaltyCorp\Multitenancy\Unit\Bridge\Laravel\Providers;
+
+use LoyaltyCorp\Multitenancy\Bridge\Laravel\Providers\ORMServiceProvider;
+use LoyaltyCorp\Multitenancy\Externals\Interfaces\ORM\EntityManagerInterface;
+use LoyaltyCorp\Multitenancy\Externals\ORM\EntityManager;
+use Tests\LoyaltyCorp\Multitenancy\TestCases\Unit\ServiceProviderTestCase;
+
+/**
+ * @covers \LoyaltyCorp\Multitenancy\Bridge\Laravel\Providers\ORMServiceProvider
+ */
+class ORMServiceProviderTest extends ServiceProviderTestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getBindings(): array
+    {
+        return [
+            EntityManagerInterface::class => EntityManager::class
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getServiceProvider(): string
+    {
+        return ORMServiceProvider::class;
+    }
+}

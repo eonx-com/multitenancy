e3fbdaec070d40dca6d3d6f1e21e37506480e39d
Ketan Padia <37789589+ketanp77@users.noreply.github.com>
2019-10-11 12:35:39 +1100

    PYMT-1307 Provider aware search (#24)
    * PYMT-1307 Add provider aware search.
    
    * PYMT-1307 Add provider aware index transformer.
    
    * PYMT-1307 Move search requirement to dev.

---
+++ tests/Stubs/Vendor/LoyaltyCorp/Search/Handlers/SearchHandlerStub.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\LoyaltyCorp\Multitenancy\Stubs\Vendor\LoyaltyCorp\Search\Handlers;
+
+use LoyaltyCorp\Search\Interfaces\EntitySearchHandlerInterface;
+
+/**
+ * @coversNothing
+ */
+final class SearchHandlerStub implements EntitySearchHandlerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getMappings(): array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSettings(): array
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getHandledClasses(): array
+    {
+        return [SearchableStub::class];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getIndexName(): string
+    {
+        return 'search-index';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSearchId(object $object)
+    {
+        return \method_exists($object, 'getSearchId') ? $object->getSearchId() : null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function transform($object = null): ?array
+    {
+        return \method_exists($object, 'toArray') ? $object->toArray() : null;
+    }
+}
---
+++ tests/Unit/Bridge/Laravel/Providers/ProviderSearchServiceProviderTest.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\LoyaltyCorp\Multitenancy\Unit\Bridge\Laravel\Providers;
+
+use EoneoPay\Externals\ORM\EntityManager;
+use EoneoPay\Externals\ORM\Interfaces\EntityManagerInterface;
+use Illuminate\Container\Container;
+use LoyaltyCorp\Multitenancy\Bridge\Laravel\Providers\ProviderSearchServiceProvider;
+use LoyaltyCorp\Multitenancy\Services\Search\Transformers\ProviderIndexTransformer;
+use LoyaltyCorp\Search\Interfaces\Transformers\IndexTransformerInterface;
+use Tests\LoyaltyCorp\Multitenancy\TestCases\Unit\ServiceProviderTestCase;
+
+/**
+ * @covers \LoyaltyCorp\Multitenancy\Bridge\Laravel\Providers\ProviderSearchServiceProvider
+ */
+final class ProviderSearchServiceProviderTest extends ServiceProviderTestCase
+{
+    /**
+     * {@inheritdoc}
+     *
+     * Set up is used in this test to bind externals entity manager which is injected
+     * in ProviderIndexTransformer.
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->bindExternalsEM();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getBindings(): array
+    {
+        return [
+            IndexTransformerInterface::class => ProviderIndexTransformer::class,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getServiceProvider(): string
+    {
+        return ProviderSearchServiceProvider::class;
+    }
+
+    /**
+     * Bind externals entity manager.
+     *
+     * @return void
+     */
+    private function bindExternalsEM(): void
+    {
+        $this->app->bind(EntityManagerInterface::class, static function (Container $app): EntityManager {
+            $entityManager = $app->make('registry')->getManager();
+
+            return new EntityManager($entityManager);
+        });
+    }
+}

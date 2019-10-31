e6bd38e783129ed1fb8f1df40ff700c0584512a5
Tim Nagel <tim@nagel.com.au>
2019-10-31 13:20:11 +1100

    Add getFillIterable to repository

--- src/Services/Webhooks/Bridge/Doctrine/Repositories/ProviderAwareActivityRepository.php
+++ src/Services/Webhooks/Bridge/Doctrine/Repositories/ProviderAwareActivityRepository.php
@@ -7,4 +7,21 @@ use LoyaltyCorp\Multitenancy\Externals\ORM\Repository;
 
 final class ProviderAwareActivityRepository extends Repository
 {
+    /**
+     * Returns a fill iterable.
+     *
+     * @return \LoyaltyCorp\Multitenancy\Services\Webhooks\Models\ProviderAwareActivityInterface[]
+     *
+     * @codeCoverageIgnore This method is exceptionally difficult to test given the incomplete
+     * doctrine setup in this libraries test suite and the fact the query builder and query
+     * inside doctrine cannot be stubbed (or replaced with mocks due to the final Query class).
+     */
+    public function getFillIterable(): iterable
+    {
+        $builder = $this->createQueryBuilder('w');
+
+        foreach ($builder->getQuery()->iterate() as $result) {
+            yield $result[0];
+        }
+    }
 }
